32fc465ec546bbdc0a944530196d88d1b4470340
Debbie Teakle <debbieteakle@gmail.com>
2019-10-07 20:31:59 +1100

    fixing faq and testimonial icons centre alignment, updating faq text

--- src/components/faqs.js
+++ src/components/faqs.js
@@ -15,32 +15,32 @@ const FAQs = () => {
 				<article className="faq">
 					<h3>How do I pay?</h3>
 					<div className="faq-icon"><FaCreditCard /></div>
-					<p>I'll send you an invoice via email. Invoices be paid safely and securely online via credit card or via direct debit transfer.</p>
+					<p>I'll send you an invoice via email which will have a link so you can pay safely and securely online via credit card or via direct debit transfer.</p>
 				</article>
 				<article className="faq">
 					<h3>What do I need for online tutoring?</h3>
 					<div className="faq-icon"><FaLaptop /></div>
-					<p>A Skype account and a computer with a microphone and webcam (most laptops have them already).</p>
+					<p>A Skype account and a computer with a microphone and webcam (most laptops have them inbuilt). You'll also need a Google account with access to Google Docs.</p>
 				</article>
 				<article className="faq">
 					<div className="faq-icon"><FaCalendarTimes /></div>
 					<h3>What if I can't attend my lesson?</h3>
-					<p>No problem, let me know you can't make our regular time and we'll reschedule.</p>
+					<p>No problem, these things happen. Let me know which lesson you can't attend and we'll reschedule it.</p>
 				</article>
 				<article className="faq">
 					<div className="faq-icon"><FaUserFriends /></div>
 					<h3>Do you tutor in person?</h3>
-					<p>Yes! My home office is a short walk from Berowra Train Station. It's air conditioned and you can use my WiFi.</p>
+					<p>Yes! My home office is a short walk from Berowra Train Station. It's air conditioned, has a nice view of my backyard and you can use my WiFi.</p>
 				</article>
 				<article className="faq">
 					<div className="faq-icon"><FaClock /></div>
 					<h3>Do I pay week by week or by school term?</h3>
-					<p>Both! I have a casual rate and a discounted rate if you pay for the whole term (10 lessons). Get in touch to learn more.</p>
+					<p>It's up to you. I offer a casual rate and a discounted rate if you pay for the whole term (10 lessons). Get in touch to learn more.</p>
 				</article>
 				<article className="faq">
 					<div className="faq-icon"><FaUserGraduate /></div>
 					<h3>Are you a qualified teacher?</h3>
-					<p>Yes! I have a Bachelor of Arts/Bachelor of Education from the University of New South Wales.</p>
+					<p>Yes, I have a Bachelor of Arts/Bachelor of Education from the University of New South Wales.</p>
 				</article>
 			</div>
 		</section>
