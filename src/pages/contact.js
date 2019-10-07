e4d7b1cd8af035749c36935752551cfc495adeaf
Debbie Teakle <debbieteakle@gmail.com>
2019-10-07 18:02:17 +1100

    added closing tag to meta element

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -10,8 +10,9 @@ const ContactPage = () => (
 		          <meta charSet="utf-8" />
 		          <title>Contact Mathew</title>
 		          <link rel="canonical" href="https://teakle.com.au/contact" />
-		          <meta name="description" content="Mathew Teakle is a qualified English teacher with 20 years of tutoring experience. 
-		          He provides private English tuition online to students all over NSW. Pay week by week or by term. Contact Mathew to discuss your English tutoring needs.">
+		          <meta name="description" content="Mathew Teakle is a qualified English teacher with 20 years 
+		          of tutoring experience. He provides private English tuition online to students all over NSW. 
+		          Pay week by week or by term. Contact Mathew to discuss your English tutoring needs." />
 		</Helmet>
 		<ContactForm />
 	</Layout>
--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ const IndexPage = () => {
 		          <title>Mathew Teakle | English Tutor</title>
 		          <link rel="canonical" href="https://teakle.com.au" />
 		          <meta name="description" content="Mathew Teakle is a qualified English teacher with 20 years of tutoring experience. 
-		          He provides private English tuition online to students all over NSW. Pay week by week or by term. Contact Mathew to discuss your English tutoring needs.">
+		          He provides private English tuition online to students all over NSW. Pay week by week or by term. Contact Mathew to discuss your English tutoring needs." />
 		        </Helmet>
 			    <Header />
 			    <Enrolments />
