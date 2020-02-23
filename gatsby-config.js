2fd3d86b8b59e29821434450d10f425c9856b1b0
Debbie Teakle <debbieteakle@gmail.com>
2020-02-23 16:21:06 +1100

    added manifest plugin to generate manifest file to improve PWA score in Lighthouse, and enable user to Add to Homescreen

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,6 +24,20 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Mathew Teakle Tutoring`,
+        short_name: `Teakle`,
+        start_url: `/`,
+        background_color: `#eee`,
+        theme_color: `#293946`,
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: `standalone`,
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         trackingId: "UA-63831923-1",
