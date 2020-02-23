1228e0f366f66245127e94bba2f4938c6a859f3b
Debbie Teakle <debbieteakle@gmail.com>
2020-02-23 12:22:05 +1100

    created recent testimonials component, added to home page with new View All button linking to testimonials page, centre aligned the button

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import "../components/global.css"
 import Header from "../components/header.js"
 import Enrolments from "../components/enrolments.js"
 import Credentials from "../components/credentials.js"
-import Testimonials from "../components/testimonials.js"
+import RecentTestimonials from "../components/recent-testimonials.js"
 import About from "../components/about.js"
 import FAQs from "../components/faqs.js"
 import Layout from "../components/layout"
@@ -28,7 +28,7 @@ const IndexPage = () => {
 			    <Header />
 			    <Enrolments />
 			    <Credentials />
-		   		<Testimonials />
+		   		<RecentTestimonials />
 		   		<About />
 		   		<FAQs />
 		  	</Layout>	
