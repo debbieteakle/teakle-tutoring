a76c288dcf6f441208b95100dbc3f2d76d8572cd
Debbie Teakle <debbieteakle@gmail.com>
2020-07-17 17:51:07 +1000

    replaced faq and testimonial global css padding with tailwindcss

--- src/components/faqs.js
+++ src/components/faqs.js
@@ -11,35 +11,35 @@ const FAQs = () => {
 	return (
 		<section className="pt-4">
 			<h2 className="text-center text-2xl">FAQs</h2>
-			<div className="flex flex-wrap justify-center">
-				<article className="faq">
+			<div className="flex flex-wrap justify-center m-2">
+				<article className="faq p-8">
 					<h3>How do I pay?</h3>
 					<div className="faq-icon"><FaCreditCard /></div>
 					<p>I'll send an invoice via email with a link, so you can pay securely online via credit card or direct debit transfer.
 </p>
 				</article>
-				<article className="faq">
+				<article className="faq p-8">
 					<h3>What do I need for online tutoring?</h3>
 					<div className="faq-icon"><FaLaptop /></div>
 					<p>A Skype account with a microphone and webcam. You'll also need a Google account with access to Google Docs.</p>
 				</article>
-				<article className="faq">
+				<article className="faq p-8">
 					<div className="faq-icon"><FaCalendarTimes /></div>
 					<h3>What if I can't attend my lesson?</h3>
 					<p>No problem, these things happen. Let me know which lesson you can't attend and we'll reschedule it. I don't make my students pay for lessons they don't have. </p>
 				</article>
-				<article className="faq">
+				<article className="faq p-8">
 					<div className="faq-icon"><FaUserFriends /></div>
 					<h3>Do you tutor in person?</h3>
 					<p>Yes! My home office is a five minute walk from a train station. We have an air conditioned office, with a nice view of the backyard and you can use our WiFi and charge your phone. 
 					During lessons we might get an unannounced visit from our dog (Bear) and it's very likely one of the kids will be playing in the backyard as well. </p>
 				</article>
-				<article className="faq">
+				<article className="faq p-8">
 					<div className="faq-icon"><FaClock /></div>
 					<h3>Do I pay week by week or by school term?</h3>
 					<p>It's up to you. I offer a casual rate and a discounted rate if you pay for the whole term (10 lessons). Get in touch to learn more.</p>
 				</article>
-				<article className="faq">
+				<article className="faq p-8">
 					<div className="faq-icon"><FaUserGraduate /></div>
 					<h3>Are you a qualified teacher?</h3>
 					<p>I have a Bachelor of Arts/Bachelor of Education (English Literature) from the University of New South Wales.</p>
--- src/components/recent-testimonials.js
+++ src/components/recent-testimonials.js
@@ -7,7 +7,7 @@ const RecentTestimonials = () => {
 		<section className="pt-4">
 			<h2 className="text-center text-2xl">HSC Student Testimonials</h2>
 			<div className="flex flex-wrap justify-center">
-				<article className="testimonial">
+				<article className="testimonial p-8">
 					<span className="testimonial-icon"><FaQuoteRight /></span>
 					<div className="testimonial-description">Learning English from you has both boosted my confidence and my understanding of English. 
 					Before, I was unsure of what I was doing or how to make sense of the new English syllabus, but after I learnt from you, I had a whole different perspective on
@@ -16,7 +16,7 @@ const RecentTestimonials = () => {
 					<p className="testimonial-author">Venkat</p>
 					<p className="testimonial-author-details">St Francis Xavier’s College, Hamilton, HSC 2019</p>
 				</article>
-				<article className="testimonial">
+				<article className="testimonial p-8">
 					<span className="testimonial-icon"><FaQuoteRight /></span>
 					<div className="testimonial-description">Mat was my tutor for years 11 and 12 and it was one of the best decisions I made. He helped improve my English skills so much. 
 					His wealth of knowledge made me really appreciate the nature of this subject. 
@@ -24,13 +24,13 @@ const RecentTestimonials = () => {
 					<p className="testimonial-author">Stella</p>
 					<p className="testimonial-author-details">Tamworth, HSC 2019</p>
 				</article>
-				<article className="testimonial">
+				<article className="testimonial p-8">
 					<span className="testimonial-icon"><FaQuoteRight /></span>
 					<div className="testimonial-description">… thank you for your help with English. I really appreciate the effort you put in and I’ll be sure to recommend you to other students if that’s okay with you!</div>
 					<p className="testimonial-author">Annie</p>
 					<p className="testimonial-author-details">Tara Anglican School for Girls, HSC 2018</p>
 				</article>
-				<article className="testimonial">
+				<article className="testimonial p-8">
 					<span className="testimonial-icon"><FaQuoteRight /></span>
 					<div className="testimonial-description">Hi Mat! First of all I'd like to thank myself for choosing you as my English tutor for three years. My decision was an excellent one and without it, 
 					I would have chosen a lesser tutor. I'd also like to thank my parents for having me in the first place so I could grow up to choose a great tutor and get a band 6 for English Advanced.
