539be64da76d0974a8b3029b814cf89b4e63d497
Debbie Teakle <debbieteakle@gmail.com>
2020-07-19 15:22:10 +1000

    adding countdown page and fixing editing main teaching areas

--- src/components/about.js
+++ src/components/about.js
@@ -4,31 +4,36 @@ import React from "react"
 const About = () => {
 	return (
 		<section className="about-section bg-gray-200" id="about">
-			<div className="about-subsection">
-				<h2>About Mathew</h2>
-				<p>B.A. B.Ed. University of New South Wales</p>
-				<p>Mathew has been tutoring English since 1998 and despite it being a part time gig 
-				for much of that time, demand for his services increased so much that he is now tutoring
-				full-time. He has four kids and is married to Debbie who works in the tech industry. 
-				(Hence this awesome website!) In his spare time Mat enjoys playing golf, going to the gym,
-				reading, writing short stories – he occasionally appears in feature films, on TV or in print
-				advertisements. He also enjoys editing copy written by his well-meaning wife… (how did I do?)</p>
-			</div>
-			<div className="about-subsection">
-					<dl>
-	  					<dt>
-	    					Main Teaching Areas
-	  					</dt>
-						  <dd class="percentage percentage-95"><span class="text">Essay Writing</span></dd>
-						  <dd class="percentage percentage-95"><span class="text">Creative Writing</span></dd>
-						  <dd class="percentage percentage-90"><span class="text">Textual Analysis</span></dd>
-						  <dd class="percentage percentage-90"><span class="text">Film</span></dd>
-						  <dd class="percentage percentage-70"><span class="text">Adult Education</span></dd>
-					</dl>
-					
+			<div className="flex flex-wrap justify-center">
+				<div className="w-full sm:w-1/2 md:w-1/2 lg:w-1/2 xl:w-1/2 mb-4 p-8 mx-4">
+					<h2 className="text-2xl">About Mathew</h2>
+					<p>B.A. B.Ed. University of New South Wales</p>
+					<p>Mathew has been tutoring English since 1998 and despite it being a part time gig 
+					for much of that time, demand for his services increased so much that he is now tutoring
+					full-time. He has four kids and is married to Debbie who works in the tech industry. 
+					(Hence this awesome website!) In his spare time Mat enjoys playing golf, going to the gym,
+					reading, writing short stories – he occasionally appears in feature films, on TV or in print
+					advertisements. He also enjoys editing copy written by his well-meaning wife… (how did I do?)</p>
+				</div>
+				<div className="w-full sm:w-1/2 md:w-1/2 lg:w-1/2 xl:w-1/2 mb-4 p-8 mx-4">
+					<h2 className="text-2xl">Main Teaching Areas</h2>
+					<ul className="pt-8 ml-10 text-lg leading-loose">
+						<li className="list-disc">Essay Writing</li>
+						<li className="list-disc">Creative Writing</li>
+						<li className="list-disc">Textual Analysis</li>
+						<li className="list-disc">Film</li>
+						<li className="list-disc">Adult Education</li>
+					</ul>
+				</div>					
 			</div>
 		</section>
 		);
 }
 
 export default About;
+
+// .about-subsection {
+// 	max-width: 500px;
+// 	padding: 30px;
+// 	flex-grow: 1;
+// }
---
+++ src/pages/HSC-countdown.js
@@ -0,0 +1,39 @@
+import React from "react"
+import { Helmet } from "react-helmet"
+import "../components/global.css"
+import Layout from "../components/layout.js"
+import { withPrefix, Link } from "gatsby"
+
+
+const Countdown = () => {
+	return (		
+		   	<Layout>
+		   	  	<Helmet>
+			          <meta charSet="utf-8" />
+			          <title>HSC Countdown to the 2020 Exams</title>
+			          <link rel="canonical" href="https://teakle.com.au/hsc-countdown" />
+			          <meta name="description" content="Mathew Teakle is a qualified English teacher with 20 years of tutoring experience. 
+			          He provides private English tuition online to students all over NSW. Pay week by week or by term. Contact Mathew to discuss your English tutoring needs." />
+					<script src={withPrefix('countdownScript.js')} type="text/javascript" />
+				</Helmet>
+			  	<section className="h-screen bg-gray-300 pt-8 mx-auto align-middle text-center">
+					<h1 className="text-4xl">HSC Exam Countdown, 2020</h1>
+						<div className="container mt-12 mx-auto">						  
+						    <p className="my-5"><span className="font-bold bg-blue-400 text-6xl p-10 rounded-lg" id="daysLeft"></span></p>	
+						    <p className="uppercase tracking-widest font-bold text-2xl">Days Left</p>
+						    <p>Day 1 of the HSC is Tuesday 20 October 2020</p>
+						    <p className="pt-10 text-2xl">You got this!</p>
+						</div>			
+				</section>
+
+
+		  	</Layout>	
+		);
+}
+export default Countdown
+
+
+
+
+
+
