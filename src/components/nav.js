fd250d2825fff1108a26c251d1091c94ac592e43
Debbie Teakle <debbieteakle@gmail.com>
2020-08-02 16:11:44 +1000

    :bug: Fixed bug where height was not given to nav element by adding height and displaying block

--- src/components/nav.js
+++ src/components/nav.js
@@ -2,13 +2,13 @@ import React from "react"
 import { Link } from "gatsby"
 const ListLink = props => (
   <li className="inline-block mr-4 mb-4">
-    <Link className="text-white" to={props.to}>{props.children}</Link>
+    <Link className="text-white block" to={props.to}>{props.children}</Link>
   </li>
 )
 
 const Nav = () => {
 	return (
-			<nav className="bg-gray-800 flex items-center justify-between flex-wrap px-6 pt-6">
+			<nav className="h-24 sm:h-24 md:h-16 lg:h-16 xl:h-16 bg-gray-800 block px-6 pt-6">
 			    <ul>
 				    <ListLink to="/">Home</ListLink>
 				    <ListLink to="/how-i-work/">How I Work</ListLink>
@@ -16,7 +16,6 @@ const Nav = () => {
 				    <ListLink to="/blog/">Blog</ListLink>				    
 				    <ListLink to="/contact/">Contact</ListLink>
 			    </ul>
-
 		    </nav>   	
 	);
 }
