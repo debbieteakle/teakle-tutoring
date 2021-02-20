1f7d2b3ca7ffc4e54d80fc35365c1131853e34c6
Debbie Teakle <debbieteakle@gmail.com>
2021-02-20 11:26:09 +1100

    hsc pen blog post

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,7 @@ import React from "react"
 const Footer = () => {
 	return (
 		<footer className="bg-gray-800 global-footer text-gray-500 text-center">
-			<p>&copy; {new Date().getFullYear()} Website by <a href="https://au.linkedin.com/in/debbieteakle">Debbie Teakle</a></p>  
+			<p>&copy; {new Date().getFullYear()}</p>  
 		</footer>
 		);
 }
