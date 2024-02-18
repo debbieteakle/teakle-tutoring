5157427b14a26382b3eb09a75cefcc2e371925f8
Debbie Teakle <debbieteakle@gmail.com>
2024-02-18 15:54:35 +1100

    Update enrolments.js

--- src/components/enrolments.js
+++ src/components/enrolments.js
@@ -3,7 +3,7 @@ import React from "react"
 const Enrolments = () => {
 	return (
 		<section className="text-center bg-orange-400">
-			<p className="leading-loose tracking-wide m-0">Accepting New Enrolments Now…</p>
+			<p className="leading-loose tracking-wide m-0"></p>
 		</section>
 		);
 }
